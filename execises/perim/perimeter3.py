5e2108e2297e8b3d72487085d4d96e8681683de6
daph-td <duong.huynh@aiesec.net>
2022-06-11 15:32:51 +0700

    store perim exercises in folder

--- execises/perim/perimeter3.py
+++ execises/perim/perimeter3.py
@@ -1,15 +1,13 @@
 import math
 
 getAngle = int(input('How many angles does the shape have? '))
-
 p_array = []
 for angle in range(getAngle):
-    xPt = int(input(f'Please input x of the angle {angle+1}: '))
-    yPt = int(input(f'Please input y of the angle {angle+1}: '))
+    xPt = float(input(f'Please input x of the angle {angle+1}: '))
+    yPt = float(input(f'Please input y of the angle {angle+1}: '))
     getCoor = (xPt, yPt)
     p_array.append(getCoor)
 
-print(p_array)
 # p1 = (-1,3)
 # p2 = (-1,-1)
 # p3 = (4,-1)
