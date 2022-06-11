0b68bf4efb72113a9546feb6c9ef23bc126d4119
daph-td <duong.huynh@aiesec.net>
2022-06-11 16:45:45 +0700

    write perim ex in java

---
+++ execises/perim/perimeter.java
@@ -0,0 +1,10 @@
+// package execises.perim;
+import java.util.Scanner ;
+
+public class perimeter {
+    public static void main(String[] args) {
+        Scanner getAngle = new Scanner(System.in);
+        // String inputPrompt = getAngle.nextLine();
+        System.out.println(getAngle);
+    }
+}
