91a5f2189c5f5beccdf936eeb2ac33f5fad1fefc
daph-td <duong.huynh@aiesec.net>
2022-06-21 19:13:48 +0700

    data types

--- execises/ex1.java
+++ execises/ex1.java
@@ -1,8 +1,15 @@
 public class ex1 {
     public static void main(String[] args) {
-        // this is a comment
-        int a = 5;
-        System.out.println(a);
+        System.out.println("Hello \"World\"");
+        // this is a single comment
+        /* this is a multi-line comment */
+        char nameChar = 'D';
+        String fullName = "Daphne";
+        int birthDay = 25;
+        double birthMonth = 1.0;
+        boolean myBool = false;
+        String message = fullName + " birthday is " + birthDay;
+        System.out.println(message);
         System.out.println("Start ...");
         System.out.println(9 + 2);
         System.out.println(9 - 2);
