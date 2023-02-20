98f91a32e3cfd5e868c04f4e061ac05f468c9307
Daph <daph@Daphs-MacBook-Pro.local>
2023-02-20 19:28:01 +0700

    exercise

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,23 @@
 public class helloWorld {
+
     public static void main(String[] args) {
+        int a = 100;
+        int b = 200;
+        System.out.print(a + b);
+
+        char alo = 'hello, '; // character
+        char alo2 = 'world';
         System.out.print("Hello, World!!!");
+        System.out.print(alo + alo2);
+
+        long c = 500;
+        double d = 3.2;
+        System.out.print(alo + alo2);
+
+        int myNum = 5;               // Integer (whole number)
+        float myFloatNum = 5.99f;    // Floating point number
+        char myLetter = 'D';         // Character
+        boolean myBool = true;       // Boolean
+        String myText = "Hello";     // String
     }
 }
